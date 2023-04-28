680828e8f1c5fd7b9a2c2eaa1a751406db065c65
Ulf Andrae <snh@ac6-102.bullx>
2023-04-28 04:35:52 +0000

    Ulf Andrae: Add vobs

--- Unpack.py
+++ Unpack.py
@@ -8,7 +8,7 @@ import yaml
 
 class Experiment:
 
- def __init__(self, name=None, path=None, mbrs=None):
+ def __init__(self, name=None, path=None, mbrs=None, is_vfld=True):
         self.name = name
         self.archive = '{}/archive/extract'.format(path) if name in path else '{}/{}/archive/extract'.format(path,name)
         if not os.path.exists(self.archive):
@@ -27,6 +27,7 @@ class Experiment:
          if len(self.mbrs) == 0 :
           self.mbrs = ['']
 
+        self.is_vfld = is_vfld
         # Testing
         self.dry = True
 
@@ -49,10 +50,13 @@ class Experiment:
    name = self.mbrs[mbr] if isinstance(self.mbrs,dict) else mbr
    t = '{}/{}/'.format(tpath,name)
    print('Target directory:',t)
-   #sp ='{}/{}'.format(self.archive,mbr)
    sp = self.archive
    print(' Scan',sp,name)
    files = find_files(sp,name)
+   if self.is_vfld :
+      files = [f for f in files if "vfld" in f]
+   else:
+      files = [f for f in files if "vobs" in f]
    nf = len(files)
    print('  found {} files'.format(nf))
    if not os.path.exists(t) and nf > 0 :
@@ -63,11 +67,16 @@ class Experiment:
 
    for f in files :
     ff = '{}/{}'.format(sp,f)
-    dtg = re.search(r'(.*)(\d{10}.tar.gz)',f).group(2).split('.')[0]
-    yy,mm,dd,hh = re.search(r'(\d{4})(\d{2})(\d{2})(\d{2})',dtg).groups()
-    fpath = '{}/{}/{}/{}/'.format(t.rstrip('/'),yy,mm,dd)
-    #print ('Search in',fpath)
-    g = glob('{}vfld*{}*'.format(fpath,dtg))
+    if self.is_vfld:
+      dtg = re.search(r'(.*)(\d{10}.tar.gz)',f).group(2).split('.')[0]
+      yy,mm,dd,hh = re.search(r'(\d{4})(\d{2})(\d{2})(\d{2})',dtg).groups()
+      fpath = '{}/{}/{}/{}/'.format(t.rstrip('/'),yy,mm,dd)
+      g = glob('{}vfld*{}*'.format(fpath,dtg))
+    else:
+      dtg = re.search(r'(.*)(\d{8}.tar.gz)',f).group(2).split('.')[0]
+      yy,mm,dd = re.search(r'(\d{4})(\d{2})(\d{2})',dtg).groups()
+      fpath = '{}/{}/{}/{}/'.format(t.rstrip('/'),yy,mm,dd)
+      g = glob('{}vobs*{}*'.format(fpath,dtg))
     if len(g) == 0 :
      print(" Unpack",f,"to",fpath)
      if not self.dry :
@@ -102,7 +111,7 @@ def find_files(path,pattern):
 
   for entry in it:
       if not entry.name.startswith('.') and entry.is_file():
-          if re.search('(.*)'+pattern+'\d{10}.tar.gz',entry.name) :
+          if re.search('(.*)'+pattern+'(\d{10}|\d{8}).tar.gz',entry.name) :
             result.append(entry.name)
       if not entry.name.startswith('.') and entry.is_dir():
           subresult = find_files(os.path.join(path,entry.name),pattern)
@@ -118,13 +127,15 @@ def run(config) :
   exp = [None]*len(config['experiments'])
   for i,e in enumerate(config['experiments']):
     members=config['experiments'][e]['members'] if 'members' in config['experiments'][e] else []
-    exp[i] = Experiment(e,config['experiments'][e]['input_path'],members)
+    is_vfld=config['experiments'][e]['is_vfld'] if 'is_vfld' in config['experiments'][e] else True
+
+    exp[i] = Experiment(e,config['experiments'][e]['input_path'],members,is_vfld)
     exp[i].dry = config['dry']
     exp[i].unpack_vfld(verdir)
 
 def main(argv) :
 
-  parser = argparse.ArgumentParser(description='Unpack vfld files')
+  parser = argparse.ArgumentParser(description='Unpack vfld/vobs files')
   parser.add_argument('-c',dest="config_file",help='Config file',required=True,default=None)
   parser.add_argument("-d", action="store_true", dest="dry", 
                       help="Dry run, check but do not fetch, send or clean anything", default=False)
